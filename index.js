d5d7e33dd7323a8231becd813a0647ded3418a14
HemaLatha0212 <hemalathapolimera02@gmail.com>
2026-04-18 09:42:52 +0530

    Added basic Express server with /hello route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/',(req,res) => {
+  res.send('welcomee!!');
+});
+
 // Simple GET endpoint
 app.get('/hello', (req, res) => {
   res.send('Hello from Day 2 server!');
